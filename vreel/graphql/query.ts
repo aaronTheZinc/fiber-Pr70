ab72c818713c8576cab03e33748b7b2d3aa0c25f
Kevin J Mosley <Kevinjmosley93@gmail.com>
2022-02-19 20:10:57 -0800

    Co-authored-by: Aaron Marsh <aaronTheZinc@users.noreply.github.com>

--- vreel/graphql/query.ts
+++ vreel/graphql/query.ts
@@ -4,13 +4,13 @@ import { User } from "../types";
 
 
 
-// export const LoginQuery = gql`
-//   query Login($email: String!, $password: String!) {
-//     login(input: { email: $email, password: $password }) {
-//       token
-//     }
-//   }
-// `;
+export const LoginQuery = gql`
+  query Login($email: String!, $password: String!) {
+    login(input: { email: $email, password: $password }) {
+      token
+    }
+  }
+`;
 const UsernameQuery = gql`
   query User($Username: String) {
     username(username: $Username) {
