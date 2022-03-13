3ccc06bc7936a2909ee137ce57840f4eb94e0511
Kevin J Mosley <Kevinjmosley93@gmail.com>
2022-03-13 01:19:04 -0800

    add to cookie logic

--- vreel/components/Shared/Header/Header.tsx
+++ vreel/components/Shared/Header/Header.tsx
@@ -1,16 +1,23 @@
+import { useRouter } from "next/router";
 import React from "react";
 
 const Header = (): JSX.Element => {
+  const router = useRouter();
   return (
     <>
       <header className="vreel-header vreel-header__wrapper">
-        <img src="/logo.svg" alt="vreel logo" className="vreel-header__logo" />
+        <img
+          onClick={() => router.push("/")}
+          src="/logo.svg"
+          alt="vreel logo"
+          className="vreel-header__logo"
+        />
         <img
           onClick={(e) => {
             const el = document.querySelector(
               ".vreel-menu.vreel-menu__wrapper"
             );
-            el.style.display = "flex";  
+            el.style.display = "flex";
           }}
           src="/menu-bars.svg"
           alt="vreel menu"
