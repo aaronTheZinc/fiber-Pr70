1e682581a33730b2167ed8cd912dd713c202ea55
Kevin J Mosley <Kevinjmosley93@gmail.com>
2022-03-30 01:00:49 -0700

    add favicon and title to pages

--- vreel/pages/_document.tsx
+++ vreel/pages/_document.tsx
@@ -16,17 +16,9 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta property="og:keywords" name="keywords" content="" />
+        <meta property="og:keywords" name="keywords" content="" />
           <meta property="og:author" name="author" content="" />
-          <link rel="icon" href="/favicon.ico" />
-          <link rel="icon" href="/favicon-32x32.png" sizes="32x32" />
-          <link rel="icon" href="/android-chrome-192x192.png" sizes="192x192" />
-          <link rel="icon" href="/android-chrome-512x512.png" sizes="512x512" />
-          <link
-            rel="apple-touch-icon"
-            href="/apple-touch-icon.png"
-            sizes="180x180"
-          />
+          <link rel="icon" href="/favicon.png" />
           {/* Global Site Tag (gtag.js) - Google Analytics */}
 
           {/* LinkedIn Insight Tag*/}
--- vreel/pages/index.tsx
+++ vreel/pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Links from "../components/Elements/Links/Links";
 import Services from "../components/Elements/Services/Services";
 import Social from "../components/Elements/Social/Social";
@@ -8,10 +9,12 @@ export default function Home({  isMobile }) {
   return (
 
     <div>
+      <Head>
+        <title>VReel Homepage</title>
+      </Head>
       <VreelSlider />
       <Links />
       <Social />
-      
       <Services isMobile={isMobile} />
       <TextArea />
     </div>
