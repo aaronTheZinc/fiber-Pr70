45005def8e5f5599245f6886c5de355948cc4b9c
Kevin J Mosley <Kevinjmosley93@gmail.com>
2022-03-30 00:59:04 -0700

    delete users from db everytime loads and change next config

--- server/database/test.go
+++ server/database/test.go
@@ -1,10 +1,12 @@
 package database
 
-func ClearAllTestUsers() {
-	// users, _ := GetAllUsernames()
+import "github.com/vreel/app/graph/model"
 
-	// for _, r := range users {
-	// 	db.Where("username = ?", r.Username).Delete(&model.UserModel{})
-	// }
+func ClearAllTestUsers() {
+	users, _ := GetAllUsernames()
+
+	for _, r := range users {
+		db.Where("username = ?", r.Username).Delete(&model.UserModel{})
+	}
 
 }
