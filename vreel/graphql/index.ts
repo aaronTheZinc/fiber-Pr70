706b24cb3fe70627716384a947ccb44af9326a4e
Aaron Marsh <aaronmarsh755@gmail.com>
2022-04-01 00:19:13 -0400

    fixed broken route

--- vreel/graphql/index.ts
+++ vreel/graphql/index.ts
@@ -7,7 +7,7 @@ import {
 
 const envType = process.env.NEXT_PUBLIC_ENV_TYPE
 
-const BASE_URL = envType == "dev" ? "http://localhost:8080" : "https://dev1.vreel.page/graphql"
+const BASE_URL = envType == "dev" ? "http://localhost:8080" : "https://dev1.vreel.page"
 console.log("[base url] ->", BASE_URL)
 console.log("env type ->", envType)
 export const client = new ApolloClient({
