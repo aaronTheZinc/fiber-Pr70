80f1dfe3eb5178ad784c6847f69b784680b3639d
Aaron Marsh <aaronmarsh755@gmail.com>
2022-04-06 21:33:15 -0400

    removed gui

--- database gui/index.js
+++
@@ -1,7 +0,0 @@
-(async () => {
-    const app = require('express')();
-    const {Noco} = require("nocodb");
-    app.use(await Noco.init({}));
-    console.log(`Visit : localhost:9090/dashboard`)    
-    app.listen(9090);
-})()
