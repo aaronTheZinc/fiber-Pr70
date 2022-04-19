77a9d4f1147fb241ad56630785be774b98b5eae0
Aaron Marsh <aaronmarsh755@gmail.com>
2022-04-19 09:29:02 -0400

    switched to server side props

--- vreel/pages/[username].tsx
+++ vreel/pages/[username].tsx
@@ -1,3 +1,4 @@
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
@@ -36,34 +37,46 @@ const Username = ({ user, isMobile }) => {
 
 export default Username;
 
-export async function getStaticPaths() {
-  try {
-    let { usernames } = await getServerAnalytics();
-    console.log("running");
-    if (!usernames) {
-      usernames = [""];
-    }
-    const paths = usernames.map((username) => {
-      console.log(`username ${username}`);
-      if (username == "") {
-        username = "--";
-      }
-      return { params: { username } };
-    });
-    return { paths, fallback: "blocking" };
-  } catch (error) {
-    console.error("ERRORRRR in [username] getStaticPaths", error);
-  }
-}
+// export async function getStaticPaths() {
+//   try {
+//     let { usernames } = await getServerAnalytics();
+//     console.log("running");
+//     if (!usernames) {
+//       usernames = [""];
+//     }
+//     const paths = usernames.map((username) => {
+//       console.log(`username ${username}`);
+//       if (username == "") {
+//         username = "--";
+//       }
+//       return { params: { username } };
+//     });
+//     return { paths, fallback: "blocking" };
+//   } catch (error) {
+//     console.error("ERRORRRR in [username] getStaticPaths", error);
+//   }
+// }
 
-export async function getStaticProps({ params }) {
-  try {
-    console.log("[locating 🔎]", params.username);
-    const user = await getUserByUsername(params.username);
+// export async function getStaticProps({ params }) {
+//   try {
+//     console.log("[locating 🔎]", params.username);
+//     const user = await getUserByUsername(params.username);
 
-    return { props: { user } };
-  } catch (error) {
-    console.log("[failed user query!! ]", error);
-    return { notFound: true };
-  }
-}
+//     return { props: { user } };
+//   } catch (error) {
+//     console.log("[failed user query!! ]", error);
+//     return { notFound: true };
+//   }
+// }
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const { username } = params;
+  const user = await getUserByUsername(username?.toString());
+  console.log("[user]", user);
+  return {
+    props: {
+      user: user,
+    },
+    notFound: !user,
+  };
+};
