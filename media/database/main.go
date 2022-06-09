49cda656886613ab3b6b1e4e3c6e72bf8f87bad2
Aaron Marsh <aaronmarsh755@gmail.com>
2022-06-09 12:39:02 -0400

    upgraded media db connection

--- media/database/main.go
+++ media/database/main.go
@@ -1,7 +1,6 @@
 package database
 
 import (
-	"log"
 	"os"
 
 	"github.com/vreel/media/models"
@@ -12,17 +11,16 @@ import (
 var db, db_init_err = databaseInit()
 
 func databaseInit() (*gorm.DB, error) {
-	env := os.Getenv("env")
-	host := "db"
-	port := "5440"
-	log.Println("connecting to db")
-	if env == "dev" || env == "" {
+	var host string
+	hostENV := os.Getenv("DB_HOST")
+	if hostENV == "" {
 		host = "localhost"
-		port = "5441"
+	} else {
+		host = hostENV
 	}
 	return gorm.Open(postgres.New(postgres.Config{
-		DSN:                  "host=" + host + " user=gorm password=gorm dbname=gorm port=" + port + " sslmode=disable TimeZone=Asia/Shanghai", // data source name, refer https://github.com/jackc/pgx
-		PreferSimpleProtocol: true,                                                                                                             // disables implicit prepared statement usage. By default pgx automatically uses the extended protocol
+		DSN:                  "host=" + host + " user=gorm password=gorm dbname=gorm port=5441 sslmode=disable TimeZone=Asia/Shanghai", // data source name, refer https://github.com/jackc/pgx
+		PreferSimpleProtocol: true,                                                                                                     // disables implicit prepared statement usage. By default pgx automatically uses the extended protocol
 	}), &gorm.Config{})
 
 }
