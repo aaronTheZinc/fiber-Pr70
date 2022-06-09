5155c22a83bd644a3d0b31240ee2cc05750ada8e
Aaron Marsh <aaronmarsh755@gmail.com>
2022-06-09 13:11:25 -0400

    recursive media server db connection

--- media/database/main.go
+++ media/database/main.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"os"
+	"time"
 
 	"github.com/vreel/media/models"
 	"gorm.io/driver/postgres"
@@ -18,10 +19,18 @@ func databaseInit() (*gorm.DB, error) {
 	} else {
 		host = hostENV
 	}
-	return gorm.Open(postgres.New(postgres.Config{
+
+	db, err := gorm.Open(postgres.New(postgres.Config{
 		DSN:                  "host=" + host + " user=gorm password=gorm dbname=gorm port=5441 sslmode=disable TimeZone=Asia/Shanghai", // data source name, refer https://github.com/jackc/pgx
 		PreferSimpleProtocol: true,                                                                                                     // disables implicit prepared statement usage. By default pgx automatically uses the extended protocol
 	}), &gorm.Config{})
+
+	if err != nil {
+		time.Sleep(5 * time.Second)
+		databaseInit()
+	}
+
+	return db, err
 
 }
 
