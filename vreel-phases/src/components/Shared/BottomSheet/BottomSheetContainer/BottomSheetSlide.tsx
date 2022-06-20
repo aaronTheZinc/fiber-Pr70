bcfad2f83db401f7da811053265db46a80693473
alasim <alasim.mail@gmail.com>
2022-06-20 21:27:20 +0600

    build error fixed for next app

--- vreel-phases/src/components/Shared/BottomSheet/BottomSheetContainer/BottomSheetSlide.tsx
+++ vreel-phases/src/components/Shared/BottomSheet/BottomSheetContainer/BottomSheetSlide.tsx
@@ -12,8 +12,6 @@ import Events from "../Events/Events";
 import Socials from "../Socials/Socials";
 import Contribute from "../Contribute/Contribute";
 import MusicLinks from "../MusicLinks/MusicLinks";
-import VideosSlider from "../VideosSlider/VideosSlider";
-import ImagesSlider from "../ImgesSlider/ImagesSlider";
 import CommonSliders from "../CommonVideoImageSlider/CommonSliders";
 
 const BottomSheetSlide: React.FC<{ data: any }> = ({ data }) => {
@@ -30,8 +28,7 @@ const BottomSheetSlide: React.FC<{ data: any }> = ({ data }) => {
       modules={[Pagination, Autoplay, Mousewheel, Navigation]}
       slidesPerView={1}
       mousewheel={true}
-      speed={1000}
-      autoplay
+      speed={300}
       direction={"vertical"}
       style={{ height: "100vh" }}
       onSwiper={(swiper) => {
@@ -50,27 +47,24 @@ const BottomSheetSlide: React.FC<{ data: any }> = ({ data }) => {
           <Links links={elements.simple_links.links} parentSwiper={swiper} />
         </SwiperSlide>
       )}
-      {/* <SwiperSlide>
+      <SwiperSlide>
         <VLinks parentSwiper={swiper} />
-      </SwiperSlide> */}
-      {/* <SwiperSlide>
+      </SwiperSlide>
+      <SwiperSlide>
         <Events parentSwiper={swiper} />
-      </SwiperSlide> */}
+      </SwiperSlide>
       {elements.socials && (
         <SwiperSlide>
           <Socials socials={elements.socials.socials} parentSwiper={swiper} />
         </SwiperSlide>
       )}
-      {/* <SwiperSlide>
+      <SwiperSlide>
         <Contribute parentSwiper={swiper} />
       </SwiperSlide>
       <SwiperSlide>
         <MusicLinks parentSwiper={swiper} />
-      </SwiperSlide> */}
-      {/* c
-      <SwiperSlide>
-        <ImagesSlider />
-      </SwiperSlide> */}
+      </SwiperSlide>
+
       {elements?.gallery?.images.length && (
         <SwiperSlide>
           <CommonSliders
