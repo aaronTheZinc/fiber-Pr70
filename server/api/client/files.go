f6ca7f42a29d700f6184eec8ed7e0b4f076cb37f
Aaron Marsh <aaronmarsh755@gmail.com>
2022-06-24 11:14:21 -0400

    added

--- server/api/client/files.go
+++ server/api/client/files.go
@@ -51,7 +51,7 @@ func GetUsersFiles(id string) (model.Files, error) {
 				fmt.Println("files-" + fmt.Sprintf("%v", files_r))
 				for _, file := range files_r.Files {
 					f := file
-					f.URI = os.Getenv("MEDIA_SERVER_FILE_ENDPOINT") + file.ID
+					f.URI = os.Getenv("MEDIA_SERVER_FILE_ENDPOINT") + "/" + file.ID
 					files.Files = append(files.Files, &f)
 				}
 				files.FileCount = len(files_r.Files)
