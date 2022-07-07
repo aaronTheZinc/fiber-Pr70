958f2178a669e32ffe4cc3bff6576312d7fa65b7
Aaron Marsh <aaronmarsh755@gmail.com>
2022-07-07 11:48:48 -0400

    fixed nil pointer err

--- server/graph/model/implements.go
+++ server/graph/model/implements.go
@@ -324,21 +324,6 @@ func (c VreelModel) ToVreel(slides []*Slide) (Vreel, error) {
 	var err error
 	var e VreelElements
 
-	if e.SimpleLinks.Hidden {
-		e.SimpleLinks = nil
-	}
-	if e.Socials.Hidden {
-		e.Socials = nil
-	}
-
-	if e.Gallery.Hidden {
-		e.Gallery = nil
-	}
-
-	if e.Videos.Hidden {
-		e.Videos = nil
-	}
-
 	// for _, sl := range c.Slides {
 	// 	t, e := database.GetSlide(sl)
 	// 	if e != nil {
@@ -349,6 +334,21 @@ func (c VreelModel) ToVreel(slides []*Slide) (Vreel, error) {
 	gErr := json.Unmarshal([]byte(c.Elements), &e)
 	if gErr != nil {
 		log.Println("im the problem!! ")
+	} else {
+		if e.SimpleLinks.Hidden {
+			e.SimpleLinks = nil
+		}
+		if e.Socials.Hidden {
+			e.Socials = nil
+		}
+
+		if e.Gallery.Hidden {
+			e.Gallery = nil
+		}
+
+		if e.Videos.Hidden {
+			e.Videos = nil
+		}
 	}
 	if gErr != nil {
 		err = gErr
