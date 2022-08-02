012f40b5e2f6ec7befd1c3abbe7b68680b58dac4
Aaron Marsh <aaronmarsh755@gmail.com>
2022-08-02 15:21:36 -0400

    .env

--- HLS-Server/src/config.ts
+++ HLS-Server/src/config.ts
@@ -1,2 +1,6 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
 export const ServerEndpoint = process.env.SERVER_ENDPOINT;
+console.log(ServerEndpoint);
 export const PORT = process.env.PORT;
