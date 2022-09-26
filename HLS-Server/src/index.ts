9061f2f1bb860891e716def4e82884467c8a1c02
Aaron Marsh <aaronmarsh755@gmail.com>
2022-09-26 15:33:38 -0400

    fixed cors streaming

--- HLS-Server/src/index.ts
+++ HLS-Server/src/index.ts
@@ -51,14 +51,14 @@ async function Server() {
         entities: [UserFile],
     });
     app.use(express.json())
-    // app.use(function (req, res, next) {
-    //   res.header("Access-Control-Allow-Origin", "*");
-    //   res.header(
-    //     "Access-Control-Allow-Headers",
-    //     "Origin, X-Requested-With, Content-Type, Accept"
-    //   );
-    //   next();
-    // });
+    app.use(function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header(
+            "Access-Control-Allow-Headers",
+            "Origin, X-Requested-With, Content-Type, Accept"
+        );
+        next();
+    });
 
     app.use(express.static(rootDir));
     app.use('/data', FilesDataRouter)
